ac9fd3516db68b3495be5fd98f00f77210c15272
FutaSuzu <qianwumeng_2cyndi@qq.com>
2017-02-28 12:13:40 +0900

    .

---
+++ BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch{
+    public static int binarySearch(int[] list, int key){
+        int low = 0;
+        int high = list.length- 1;
+
+        while(high>=low){
+            int mid = (low + high) / 2;
+            if(key < list[mid])
+                high = mid - 1;
+            else if (key == list[mid])
+                return mid;
+            else
+                low = mid + 1;
+        }
+        return - low - 1;
+    }
+}
---
+++ SelectionSort.java
@@ -0,0 +1,5 @@
+public class SelectionSort{
+    public static void selectionSort(double[] list){
+        for(int i = 0; i < list.length - 1;i++)
+    }
+}
