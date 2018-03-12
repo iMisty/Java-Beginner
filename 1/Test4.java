3b7247444e0b7950f22846a7086c968dcb4ddd76
JingYi <qianwumeng_2cyndi@qq.com>
2018-03-12 22:10:59 +0800

    1-5
    C1 1~5

---
+++ 1/Test4.java
@@ -0,0 +1,9 @@
+public class Test4{
+    public static void main(String[] args){
+        System.out.println("a\ta^2\ta^3");
+        System.out.println("1\t1\t1");
+        System.out.println("2\t4\t8");
+        System.out.println("3\t9\t27");
+        System.out.println("4\t16\t64");
+    }
+}
---
+++ 1/Test5.java
@@ -0,0 +1,6 @@
+public class Test5{
+    public static void main(String[] args){
+        double i = (9.5*4.5-2.5*3) / (45.5-3.5);
+        System.out.println("i:" + i);
+    }
+}
