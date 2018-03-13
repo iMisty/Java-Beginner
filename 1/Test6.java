475ff040ab1b8f376d6f834dc781e7f33e8d3f58
JingYi <qianwumeng_2cyndi@qq.com>
2018-03-13 22:44:58 +0800

    6~8

---
+++ 1/Test6.java
@@ -0,0 +1,9 @@
+public class Test6{
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i = 0;i < 10;i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ 1/Test7.java
@@ -0,0 +1,9 @@
+public class Test7{
+    public static void main(String[] args){
+        double p = 4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11);
+        System.out.println(p);
+
+        double q = 4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13);
+        System.out.println(q);
+    }
+}
