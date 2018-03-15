65209472c4517b8a511f00ee8f123b9edaa0ab95
JingYi <qianwumeng_2cyndi@qq.com>
2018-03-15 23:22:12 +0800

    Upgrade 1-11

--- 1/Test11.java
+++ 1/Test11.java
@@ -11,7 +11,7 @@ public class Test11 {
             int dead = year * days * 24 * 60 * 60 /13;
             int in = year * days * 24 * 60 * 60 / 45;
             int sum = people + both - dead + in;
-            System.out.println("美國在第"+year+ "年時的人口數是："+sum);
+            System.out.println("The population in " + year + " years is " + sum);
         }
     }
 }
