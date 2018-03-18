405f974797564b28dc69e738f0f1fcdefe8b5dec
JingYi <qianwumeng_2cyndi@qq.com>
2018-03-18 20:46:06 +0800

    C2 20

---
+++ 2/_20.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class _20{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter balance and interest rate (e.g., 3 for 3%):");
+        double balance = in.nextDouble();
+        double rate = in.nextDouble();
+        double interest = balance * (rate / 1200);
+        System.out.println("The interest is " + interest);
+    }
+}
