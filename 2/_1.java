49f1c07e24adbc3eab37d62b0ec50337dfc397d2
JingYi <qianwumeng_2cyndi@qq.com>
2018-03-19 23:04:28 +0800

    Upgrade C2 1~3

--- 2/_1.java
+++ 2/_1.java
@@ -1,11 +1,15 @@
 import java.util.*;
-
 public class _1{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a degree in Celsius:");
-        double t = input.nextDouble();
-        double f = (9.0 / 5) * t +32;
-        System.out.println(t+" Celsius is "+f+" Fahrenheit");
+        try{
+            System.out.print("Enter a degree in Celsius:");
+            double t = input.nextDouble();
+            double f = (9.0 / 5) * t +32;
+            System.out.println(t+" Celsius is "+f+" Fahrenheit");
+        }
+        finally{
+            input.close();
+        }
     }
 }
--- 2/_3.java
+++ 2/_3.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class _3{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter a value of feet:");
-        double feet = in.nextDouble();
-        double meters = 0.305 * feet;
-        System.out.println(feet+" feet is "+meters+" meters");
+        try{
+            System.out.print("Enter a value of feet:");
+            double feet = in.nextDouble();
+            double meters = 0.305 * feet;
+            System.out.println(feet+" feet is "+meters+" meters");
+        }
+        finally{
+            in.close();
+        }
     }
 }
