45c1d402ab029091ec499feac3720f18d3fa6776
JingYi <qianwumeng_2cyndi@qq.com>
2018-03-21 20:48:53 +0800

    Upgrade C2 17

--- 2/_17.java
+++ 2/_17.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class _17{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the temperature in Fahrenheit between -58F and 41F:");
-        double temp = in.nextDouble();
-        if(temp < -58 || temp > 41){
-            System.out.println("Error");
-        }else{
-            System.out.print("Enter the wind speed (>=2) in miles per hour:");
-            double speed = in.nextDouble();
+        try{
+            System.out.print("Enter the temperature in Fahrenheit between -58F and 41F:");
+            double temp = in.nextDouble();
+            if(temp < -58 || temp > 41){
+                System.out.println("Error");
+            }else{
+                System.out.print("Enter the wind speed (>=2) in miles per hour:");
+                double speed = in.nextDouble();
             if(speed < 2){
                 System.out.println("Error");
             }else{
@@ -17,6 +18,9 @@ public class _17{
                 System.out.println("The wind chill index is " + _t);
             }
         }
-
+    }
+    finally{
+        in.close();
+    }
     }
 }
