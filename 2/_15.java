b1952e6ae1afce8873ad38066288bf7d8b3276ba
Jingyi21 <kannadukishizu@hotmail.co.jp>
2018-03-23 22:31:48 +0800

    Upgrade C2 15

--- 2/_15.java
+++ 2/_15.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class _15{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter x1 and y1:");
+	try{
+	System.out.print("Enter x1 and y1:");
         double x1 = in.nextDouble();
         double y1 = in.nextDouble();
         System.out.print("Enter x2 and y2:");
@@ -14,5 +15,9 @@ public class _15{
         double a = a1 + a2;
         double distance = Math.pow(a, 0.5);
         System.out.println("The distance between the two points is " + distance);
+	}
+	finally{
+	    in.close();
+	}
     }
 }
