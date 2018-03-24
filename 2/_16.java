5c594fce98592b4bb3598f3d33d70e1e3c54b87b
Jingyi21 <kannadukishizu@hotmail.co.jp>
2018-03-24 21:17:28 +0800

    Upgrade C2 16

--- 2/_16.java
+++ 2/_16.java
@@ -3,10 +3,15 @@ import java.util.*;
 public class _16{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the side:");
-        double side = in.nextDouble();
+        try{
+            System.out.print("Enter the side:");
+            double side = in.nextDouble();
 
-        double area = (3 * Math.pow(3, 0.5) / 2) * Math.pow(side, 2);
-        System.out.println("The area of the hexagon is " + area);
+            double area = (3 * Math.pow(3, 0.5) / 2) * Math.pow(side, 2);
+            System.out.println("The area of the hexagon is " + area);
+        }
+        finally{
+            in.close();
+        }
     }
 }
