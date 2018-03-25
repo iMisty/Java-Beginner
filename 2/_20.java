adbdd02b7455f14b9f207e09ec9f834760677288
Jingyi21 <lin.miya.moe@gmail.com>
2018-03-25 23:51:48 +0800

    C2 20

--- 2/_20.java
+++ 2/_20.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class _20{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter balance and interest rate (e.g., 3 for 3%):");
-        double balance = in.nextDouble();
-        double rate = in.nextDouble();
-        double interest = balance * (rate / 1200);
-        System.out.println("The interest is " + interest);
+        try{
+            System.out.print("Enter balance and interest rate (e.g., 3 for 3%):");
+            double balance = in.nextDouble();
+            double rate = in.nextDouble();
+            double interest = balance * (rate / 1200);
+            System.out.println("The interest is " + interest);
+        }
+        finally{
+            in.close();
+        }
     }
 }
