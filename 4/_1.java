a10898463fad2495c9390e916797aa5e815d626b
Jingyi21 <lin.miya.moe@gmail.com>
2018-04-09 22:42:15 +0800

    C4 1

--- 4/_1.java
+++ 4/_1.java
@@ -5,9 +5,10 @@ public class _1{
         Scanner in = new Scanner(System.in);
         try{
             System.out.print("Enter the length from the center to a vertex: ");
-            double vertex = in.nextDouble();
-            double s = 2 * vertex * Math.sin(3.14 / 5);
-            double square = (5 * Math.pow(s, 2)) / 4 * Math.tan(3.14 / 5);
+            double r = in.nextDouble();
+            double pi = Math.PI / 5;
+            double s = 2 * r * (Math.sin(pi));
+            double square = (5 * Math.pow(s, 2)) / (4 * Math.tan(pi));
             System.out.println("The area of the pentagon is " + square);
         }
         finally{
